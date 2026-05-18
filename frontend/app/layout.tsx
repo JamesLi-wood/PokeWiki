8e6dc2338c417e0ecfca5a8c74c3d6ca5924415f
james <jamesliwood@gmail.com>
2026-05-18 04:55:22 -0400

    added mantine provider and styles

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { MantineProvider } from "@mantine/core";
+import "@mantine/core/styles.css";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -27,7 +29,9 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <MantineProvider>{children}</MantineProvider>
+      </body>
     </html>
   );
 }
