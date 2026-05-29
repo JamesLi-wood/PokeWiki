e6cfc654e6f1a9d0360566e7982f9711ea58c6c4
james <jamesliwood@gmail.com>
2026-05-29 04:24:05 -0400

    data for game version

---
+++ frontend/utils/gameVersion.tsx
@@ -0,0 +1,101 @@
+const gameVersion = {
+  rubySapphire: {
+    version: "generation-iii/ruby-sapphire",
+    regionalDex: ["https://pokeapi.co/api/v2/pokedex/4/"],
+    limit: 386,
+  },
+  fireredLeafgreen: {
+    version: "generation-iii/firered-leafgreen",
+    regionalDex: ["https://pokeapi.co/api/v2/pokedex/2/"],
+    limit: 386,
+  },
+  emerald: {
+    version: "generation-iii/emerald",
+    regionalDex: ["https://pokeapi.co/api/v2/pokedex/4/"],
+    limit: 386,
+  },
+  diamondPearl: {
+    version: "generation-iv/diamond-pearl",
+    regionalDex: ["https://pokeapi.co/api/v2/pokedex/5/"],
+    limit: 493,
+  },
+  platinum: {
+    version: "generation-iv/platinum",
+    regionalDex: ["https://pokeapi.co/api/v2/pokedex/6/"],
+    limit: 493,
+  },
+  heartgoldSoulsilver: {
+    version: "generation-iv/heartgold-soulsilver",
+    regionalDex: ["https://pokeapi.co/api/v2/pokedex/7/"],
+    limit: 493,
+  },
+  blackWhite: {
+    version: "generation-v/black-white",
+    regionalDex: ["https://pokeapi.co/api/v2/pokedex/8/"],
+    limit: 649,
+  },
+  black2White2: {
+    version: "generation-v/black-white",
+    regionalDex: ["https://pokeapi.co/api/v2/pokedex/9/"],
+    limit: 649,
+  },
+  xy: {
+    version: "generation-vi/x-y",
+    regionalDex: [
+      "https://pokeapi.co/api/v2/pokedex/12/",
+      "https://pokeapi.co/api/v2/pokedex/13/",
+      "https://pokeapi.co/api/v2/pokedex/14/",
+    ],
+    limit: 721,
+  },
+  omegarubyAlphasapphire: {
+    version: "generation-vi/omegaruby-alphasapphire",
+    regionalDex: ["https://pokeapi.co/api/v2/pokedex/15/"],
+    limit: 721,
+  },
+  sunMoon: {
+    version: "generation-vii/ultra-sun-ultra-moon",
+    regionalDex: [
+      "https://pokeapi.co/api/v2/pokedex/17/",
+      "https://pokeapi.co/api/v2/pokedex/18/",
+      "https://pokeapi.co/api/v2/pokedex/19/",
+      "https://pokeapi.co/api/v2/pokedex/20/",
+    ],
+    limit: 807,
+  },
+  ultrasunUltramoon: {
+    version: "generation-vii/ultra-sun-ultra-moon",
+    regionalDex: [
+      "https://pokeapi.co/api/v2/pokedex/22/",
+      "https://pokeapi.co/api/v2/pokedex/23/",
+      "https://pokeapi.co/api/v2/pokedex/24/",
+      "https://pokeapi.co/api/v2/pokedex/25/",
+    ],
+    limit: 807,
+  },
+  swordShield: {
+    version: "generation-ix/scarlet-violet",
+    regionalDex: [
+      "https://pokeapi.co/api/v2/pokedex/27/",
+      "https://pokeapi.co/api/v2/pokedex/28/",
+      "https://pokeapi.co/api/v2/pokedex/29/",
+    ],
+    limit: 898,
+  },
+  brilliantdiamondShiningpearl: {
+    version: "generation-viii/brilliant-diamond-shining-pearl",
+    regionalDex: ["https://pokeapi.co/api/v2/pokedex/5/"],
+    limit: 493,
+  },
+  scarletViolet: {
+    version: "generation-ix/scarlet-violet",
+    regionalDex: [
+      "https://pokeapi.co/api/v2/pokedex/31/",
+      "https://pokeapi.co/api/v2/pokedex/32/",
+      "https://pokeapi.co/api/v2/pokedex/33/",
+    ],
+    limit: 1025,
+  },
+};
+
+export default gameVersion;
