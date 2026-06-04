07ae9f2bbe2b252552b64d7e7b607397f674a66d
james <jamesliwood@gmail.com>
2026-06-04 02:07:16 -0400

    added bdsp pokedex

---
+++ frontend/app/bdsp/pokedex/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+import Pokedex from "@/components/pokedex";
+import { useMediaQuery } from "@mantine/hooks";
+
+const Page = () => {
+  const isMobile = useMediaQuery("(max-width: 768px)");
+
+  return (
+    <div>
+      <div className={`${isMobile ? "w-full" : "w-[80%]"} mx-auto`}>
+        <Pokedex version="brilliantdiamondShiningpearl" />
+      </div>
+    </div>
+  );
+};
+
+export default Page;
