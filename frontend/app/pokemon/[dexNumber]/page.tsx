6e4108bf4b482e4c890e670bfa85c64498e7a65d
james <jamesliwood@gmail.com>
2026-06-15 03:48:17 -0400

    revert for now

--- frontend/app/pokemon/[dexNumber]/page.tsx
+++ frontend/app/pokemon/[dexNumber]/page.tsx
@@ -1,27 +1,11 @@
 "use client";
 import { useParams } from "next/navigation";
-import { getPokemon } from "@/lib/getPokemon";
-import { useQuery } from "@tanstack/react-query";
 
 const Page = () => {
   const slug = useParams().dexNumber;
   if (typeof slug !== "string") return <div>ERROR</div>;
 
-  const { isLoading, error, data } = useQuery({
-    queryKey: ["pokemon", slug],
-    queryFn: () => getPokemon(slug),
-    staleTime: Infinity,
-  });
-
-  if (error) return <div>ERROR</div>;
-
-  if (isLoading) return <div>LOADING</div>;
-
-  return (
-    <div>
-      <div>{data.name}</div>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Page;
