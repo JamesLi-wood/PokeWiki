4c2f90196c898b887edc38d56c998a204c0eaf98
james <jamesliwood@gmail.com>
2026-06-19 03:49:29 -0400

    removed usePokedex logic

--- frontend/components/pokedex.tsx
+++ frontend/components/pokedex.tsx
@@ -1,20 +1,10 @@
-import { useState } from "react";
 import { useRouter } from "next/navigation";
-import {
-  Card,
-  Image,
-  Text,
-  Badge,
-  Flex,
-  Skeleton,
-  SegmentedControl,
-} from "@mantine/core";
+import useLoadPokemon from "@/hooks/useLoadPokemon";
+import { Card, Image, Text, Badge, Flex, Skeleton } from "@mantine/core";
 import { useMediaQuery } from "@mantine/hooks";
 import capitalizeFirstLetter from "@/utils/capitalizeFirstLetter";
 import typeColors from "@/utils/typeColors";
 import gameVersion from "@/utils/gameVersion";
-import usePokedex from "@/hooks/usePokedex";
-import useLoadPokemon from "@/hooks/useLoadPokemon";
 
 type pkmnType = {
   slot: number;
@@ -31,30 +21,27 @@ type pkmnData = {
   pastTypes: [];
 };
 
-type dexVersion = "regional" | "national";
+type entries = {
+  name: string;
+  url: string;
+};
 
-const Pokedex = ({ version }: { version: keyof typeof gameVersion }) => {
+const Pokedex = ({
+  version,
+  dexKey,
+  entries,
+}: {
+  version: keyof typeof gameVersion;
+  dexKey: string;
+  entries: entries[];
+}) => {
   const pokedex = gameVersion[version];
-  const [dexVersion, setDexVersion] = useState<dexVersion>("regional");
   const isMobile = useMediaQuery("(max-width: 768px)");
   const router = useRouter();
-  const { natDex, regDex } = usePokedex(version);
-  const {
-    pokemons: nationalMons,
-    isLoading: natLoading,
-    paginateUp,
-    paginateDown,
-  } = useLoadPokemon("national", natDex!);
-  const {
-    pokemons: regionalMons,
-    isLoading: regLoading,
-    paginateUp: regPaginateUp,
-    paginateDown: regPaginateDown,
-  } = useLoadPokemon(version, regDex!);
-
-  const switchDex = () => {
-    setDexVersion((prev) => (prev === "regional" ? "national" : "regional"));
-  };
+  const { pokemons, isLoading, paginateUp, paginateDown } = useLoadPokemon(
+    dexKey,
+    entries,
+  );
 
   const LoadTypes = ({ types }: { types: pkmnType[] }) => {
     return types.map((data) => (
@@ -103,7 +90,7 @@ const Pokedex = ({ version }: { version: keyof typeof gameVersion }) => {
     pokemon,
     badgeNumber,
   }: {
-    pokemon: pkmnData | null;
+    pokemon: pkmnData;
     badgeNumber: number;
   }) => {
     return (
@@ -120,7 +107,6 @@ const Pokedex = ({ version }: { version: keyof typeof gameVersion }) => {
         <Image
           src={`${pokedex.sprite}/${pokemon?.entryNumber}.png`}
           alt={pokemon?.name}
-          className="mx-auto"
           h={`${isMobile ? "auto" : "6rem"}`}
           w={`${isMobile ? "6rem" : "auto"}`}
           fit="contain"
@@ -128,7 +114,7 @@ const Pokedex = ({ version }: { version: keyof typeof gameVersion }) => {
         <Flex
           direction="column"
           justify="center"
-          align={`${isMobile ? "flex-start" : "center"}`}
+          align="center"
           className={`${isMobile ? "ml-4" : "mt-2"}`}
         >
           <Flex
@@ -140,7 +126,6 @@ const Pokedex = ({ version }: { version: keyof typeof gameVersion }) => {
             <Badge variant="outline" size="lg" color="rgb(0, 0, 0)">
               {`#${badgeNumber}`}
             </Badge>
-
             {pokemon && <Text>{capitalizeFirstLetter(pokemon.name)}</Text>}
           </Flex>
           <Flex direction="row" gap="0.5rem">
@@ -174,77 +159,32 @@ const Pokedex = ({ version }: { version: keyof typeof gameVersion }) => {
   };
 
   return (
-    <div className="flex flex-col items-center mt-5">
-      <SegmentedControl
-        value={dexVersion}
-        onChange={switchDex}
-        data={[
-          { label: "Regional", value: "regional" },
-          { label: "National", value: "national" },
-        ]}
-        size="md"
-        px="1rem"
-        py="0.5rem"
-      />
-      <div className="flex justify-center flex-wrap gap-3 my-5">
-        {regLoading || natLoading ? (
-          Array(50)
-            .fill(null)
-            .map((_, idx) => {
-              return <SkeletonPokemonCard key={idx} />;
-            })
-        ) : dexVersion == "regional" ? (
-          <div>
-            <div className="flex justify-center flex-wrap gap-3 mb-5">
-              {regionalMons?.map((pokemon, idx: number) => {
-                if (typeof pokemon == "string") {
-                  return (
-                    <div key={idx} className="w-full text-center">
-                      {pokemon}
-                    </div>
-                  );
-                } else {
-                  return (
-                    <PokemonCard
-                      key={pokemon.entryNumber}
-                      pokemon={pokemon}
-                      badgeNumber={pokemon.entryNumber}
-                    />
-                  );
-                }
-              })}
-            </div>
-            <div>
-              <button onClick={regPaginateDown}>PREV</button>
-              <button onClick={regPaginateUp}>NEXT</button>
-            </div>
+    <div className="flex justify-center flex-wrap gap-3 my-5">
+      {isLoading ? (
+        Array(50)
+          .fill(null)
+          .map((_, idx) => {
+            return <SkeletonPokemonCard key={idx} />;
+          })
+      ) : (
+        <div>
+          <div className="flex justify-center flex-wrap gap-3 mb-5">
+            {pokemons?.map((pokemon) => {
+              return (
+                <PokemonCard
+                  key={pokemon.entryNumber}
+                  pokemon={pokemon}
+                  badgeNumber={pokemon.entryNumber}
+                />
+              );
+            })}
           </div>
-        ) : (
           <div>
-            <div className="flex justify-center flex-wrap gap-3 mb-5">
-              {nationalMons?.map((pokemon) => {
-                if (typeof pokemon == "string")
-                  return (
-                    <div key={pokemon} className="w-full text-center">
-                      {pokemon}
-                    </div>
-                  );
-                return (
-                  <PokemonCard
-                    key={pokemon.entryNumber}
-                    pokemon={pokemon}
-                    badgeNumber={pokemon.entryNumber}
-                  />
-                );
-              })}
-            </div>
-            <div>
-              <button onClick={paginateDown}>PREV</button>
-              <button onClick={paginateUp}>NEXT</button>
-            </div>
+            <button onClick={paginateDown}>PREV</button>
+            <button onClick={paginateUp}>NEXT</button>
           </div>
-        )}
-      </div>
+        </div>
+      )}
     </div>
   );
 };
