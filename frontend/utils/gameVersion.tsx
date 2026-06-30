7428297d81686feada00ea4ce4ab260c2f3edcb0
james <jamesliwood@gmail.com>
2026-06-30 02:41:50 -0400

    added title and generation and removed version key

--- frontend/utils/gameVersion.tsx
+++ frontend/utils/gameVersion.tsx
@@ -1,62 +1,71 @@
 const gameVersion = {
   rs: {
-    version: "generation-iii/ruby-sapphire",
+    title: "Ruby / Sapphire",
+    generation: "generation-iii",
     regionalDex: ["https://pokeapi.co/api/v2/pokedex/4/"],
     sprite:
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-iii/ruby-sapphire",
     limit: 386,
   },
   frlg: {
-    version: "generation-iii/firered-leafgreen",
+    title: "Firered / Leafgreen",
+    generation: "generation-iii",
     regionalDex: ["https://pokeapi.co/api/v2/pokedex/2/"],
     sprite:
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-iii/firered-leafgreen",
     limit: 386,
   },
   emerald: {
-    version: "generation-iii/emerald",
+    title: "Emerald",
+    generation: "generation-iii",
     regionalDex: ["https://pokeapi.co/api/v2/pokedex/4/"],
     sprite:
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-iii/emerald",
     limit: 386,
   },
   dp: {
-    version: "generation-iv/diamond-pearl",
+    title: "Diamond / Pearl",
+    generation: "generation-iv",
     regionalDex: ["https://pokeapi.co/api/v2/pokedex/5/"],
     sprite:
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-iv/diamond-pearl",
     limit: 493,
   },
   platinum: {
-    version: "generation-iv/platinum",
+    title: "Platinum",
+    generation: "generation-iv",
     regionalDex: ["https://pokeapi.co/api/v2/pokedex/6/"],
     sprite:
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-iv/platinum/",
     limit: 493,
   },
   hgss: {
-    version: "generation-iv/heartgold-soulsilver",
+    title: "Heartgold / Soulsilver",
+    generation: "generation-iv",
     regionalDex: ["https://pokeapi.co/api/v2/pokedex/7/"],
     sprite:
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-iv/heartgold-soulsilver",
     limit: 493,
   },
   bw: {
-    version: "generation-v/black-white",
+    title: "Black / White",
+    generation: "generation-v",
     regionalDex: ["https://pokeapi.co/api/v2/pokedex/8/"],
     sprite:
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white",
     limit: 649,
   },
   bw2: {
-    version: "generation-v/black-white",
+    title: "Black2 / White2",
+    generation: "generation-v",
     regionalDex: ["https://pokeapi.co/api/v2/pokedex/9/"],
     sprite:
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/",
     limit: 649,
   },
   xy: {
-    version: "generation-vi/omegaruby-alphasapphire",
+    title: "X / Y",
+    generation: "generation-vi",
     regionalDex: [
       "https://pokeapi.co/api/v2/pokedex/12/",
       "https://pokeapi.co/api/v2/pokedex/13/",
@@ -67,14 +76,16 @@ const gameVersion = {
     limit: 721,
   },
   oras: {
-    version: "generation-vi/omegaruby-alphasapphire",
+    title: "Omegaruby / Alphasapphire",
+    generation: "generation-vi",
     regionalDex: ["https://pokeapi.co/api/v2/pokedex/15/"],
     sprite:
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-vi/omegaruby-alphasapphire",
     limit: 721,
   },
   sm: {
-    version: "generation-vii/ultra-sun-ultra-moon",
+    title: "Sun / Moon",
+    generation: "generation-vii",
     regionalDex: [
       "https://pokeapi.co/api/v2/pokedex/17/",
       "https://pokeapi.co/api/v2/pokedex/18/",
@@ -86,7 +97,8 @@ const gameVersion = {
     limit: 807,
   },
   usum: {
-    version: "generation-vii/ultra-sun-ultra-moon",
+    title: "Ultra Sun / Ultra Moon",
+    generation: "generation-vii",
     regionalDex: [
       "https://pokeapi.co/api/v2/pokedex/22/",
       "https://pokeapi.co/api/v2/pokedex/23/",
@@ -98,7 +110,8 @@ const gameVersion = {
     limit: 807,
   },
   swsh: {
-    version: "generation-ix/scarlet-violet",
+    title: "Sword / Shield",
+    generation: "generation-viii",
     regionalDex: [
       "https://pokeapi.co/api/v2/pokedex/27/",
       "https://pokeapi.co/api/v2/pokedex/28/",
@@ -109,14 +122,16 @@ const gameVersion = {
     limit: 898,
   },
   bdsp: {
-    version: "generation-viii/brilliant-diamond-shining-pearl",
+    title: "Brilliant Diamond / Shining Pearl",
+    generation: "generation-viii",
     regionalDex: ["https://pokeapi.co/api/v2/pokedex/5/"],
     sprite:
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-viii/brilliant-diamond-shining-pearl",
     limit: 493,
   },
   sv: {
-    version: "generation-ix/scarlet-violet",
+    title: "Scarlet / Violet",
+    generation: "generation-ix",
     regionalDex: [
       "https://pokeapi.co/api/v2/pokedex/31/",
       "https://pokeapi.co/api/v2/pokedex/32/",
