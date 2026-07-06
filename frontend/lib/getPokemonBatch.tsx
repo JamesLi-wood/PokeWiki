b8008516696a0cdcf8fdc4d73a0cc9e28957b9b6
james <jamesliwood@gmail.com>
2026-07-06 04:52:00 -0400

    moved code over from getPokemon to here

---
+++ frontend/lib/getPokemonBatch.tsx
@@ -0,0 +1,33 @@
+type entries = {
+  name: string;
+  url: string;
+};
+
+/* 
+  entries.name will not be used due to some varients
+  having different names ex: rotom-wash
+*/
+export const getPokemonBatch = async (
+  page: number,
+  entries: entries[],
+  batch: number,
+) => {
+  const head = page * batch;
+  let tail = head + batch;
+
+  const nthPage = Promise.all(
+    entries.slice(head, tail).map(async (pokemon) => {
+      const res = await fetch(pokemon.url);
+      const data = await res.json();
+
+      return {
+        name: data.species.name,
+        entryNumber: data.id,
+        currentTypes: data.types,
+        pastTypes: data.past_types.length > 0 ? data.past_types[0].types : [],
+      };
+    }),
+  );
+
+  return nthPage;
+};
