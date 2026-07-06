5a02fec06f83acdeef36dbc5c54cde872c36efcc
james <jamesliwood@gmail.com>
2026-07-06 04:54:14 -0400

    code now fetches pokemon data instead of batching specific data

--- frontend/lib/getPokemon.tsx
+++ frontend/lib/getPokemon.tsx
@@ -1,33 +1,7 @@
-type entries = {
-  name: string;
-  url: string;
-};
+export const getPokemon = async (name: string) => {
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+  if (!response.ok) throw new Error("Pokemon not found.");
+  const data = response.json();
 
-/* 
-  entries.name will not be used due to some species
-  having different forms ex: rotom-wash
-*/
-export const getPokemon = async (
-  page: number,
-  entries: entries[],
-  batch: number,
-) => {
-  const head = page * batch;
-  let tail = head + batch;
-
-  const nthPage = Promise.all(
-    entries.slice(head, tail).map(async (pokemon) => {
-      const res = await fetch(pokemon.url);
-      const data = await res.json();
-
-      return {
-        name: data.species.name,
-        entryNumber: data.id,
-        currentTypes: data.types,
-        pastTypes: data.past_types.length > 0 ? data.past_types[0].types : [],
-      };
-    }),
-  );
-
-  return nthPage;
+  return data;
 };
