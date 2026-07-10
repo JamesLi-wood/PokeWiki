2871012b87f023c309227a571954dae1175b7c18
james <jamesliwood@gmail.com>
2026-07-10 03:19:10 -0400

    added type to query

--- frontend/hooks/useGetPokemon.tsx
+++ frontend/hooks/useGetPokemon.tsx
@@ -1,13 +1,19 @@
 import { useQuery } from "@tanstack/react-query";
 import { getPokemon } from "@/lib/getPokemon";
+import { PokemonData } from "@/types/pokemonData";
 
 export default function useGetPokemon(name: string) {
-  const { data, isLoading, error, isError } = useQuery({
+  const {
+    data: pokemon,
+    isLoading,
+    error,
+    isError,
+  } = useQuery<PokemonData>({
     queryKey: ["pokemon", name],
     queryFn: () => getPokemon(name),
     staleTime: Infinity,
     retry: false,
   });
 
-  return { data, error, isError, isLoading };
+  return { pokemon, error, isError, isLoading };
 }
