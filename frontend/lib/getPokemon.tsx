fd4591a8efedd9ca94bfe80c9ec03fdfc4ede6e8
james <jamesliwood@gmail.com>
2026-07-13 03:45:02 -0400

    fetching additional information

--- frontend/lib/getPokemon.tsx
+++ frontend/lib/getPokemon.tsx
@@ -1,7 +1,23 @@
-export const getPokemon = async (name: string) => {
-  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
-  if (!response.ok) throw new Error("Pokemon not found.");
-  const data = response.json();
+import { PokemonData } from "@/types/pokemonData";
+import { PokemonSpecies } from "@/types/pokemonSpecies";
 
-  return data;
+type PokemonResponse = {
+  pokemonSpecies: PokemonSpecies;
+  pokemonData: PokemonData;
+};
+
+export const getPokemon = async (name: string): Promise<PokemonResponse> => {
+  const speciesResponse = await fetch(
+    `https://pokeapi.co/api/v2/pokemon-species/${name}`,
+  );
+  if (!speciesResponse.ok) throw new Error("Pokemon not found.");
+
+  const pokemonSpecies = await speciesResponse.json();
+
+  const dataResponse = await fetch(
+    `https://pokeapi.co/api/v2/pokemon/${pokemonSpecies.id}`,
+  );
+  const pokemonData = await dataResponse.json();
+
+  return { pokemonSpecies, pokemonData };
 };
