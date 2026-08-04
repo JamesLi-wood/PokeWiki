0029ab4444350600717bd3d1eb559f24547126fd
james <jamesliwood@gmail.com>
2026-08-04 04:18:56 -0400

    fetching move data

---
+++ frontend/lib/getMove.tsx
@@ -0,0 +1,11 @@
+import { MoveData } from "@/types/moveData";
+
+type MoveType = {
+  moveData: MoveData;
+};
+
+export const getMove = async (url: string): Promise<MoveType> => {
+  const moveResponse = await fetch(url);
+  const moveData = await moveResponse.json();
+  return moveData;
+};
