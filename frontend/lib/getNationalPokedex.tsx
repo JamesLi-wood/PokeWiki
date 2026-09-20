7e565e239537d1b0d5931c016bd88dd4db900128
james <jamesliwood@gmail.com>
2026-09-20 02:55:39 -0400

    fetching more dynamic

--- frontend/lib/getNationalPokedex.tsx
+++ frontend/lib/getNationalPokedex.tsx
@@ -1,5 +1,17 @@
+type entryData = {
+  entry_number: number;
+  pokemon_species: {
+    name: string;
+    url: string;
+  };
+};
+
 export const nationalPokedex = async () => {
-  const res = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=1025`);
+  const res = await fetch("https://pokeapi.co/api/v2/pokedex/1");
   const data = await res.json();
-  return { title: "National Pokedex", entries: data.results };
+  const entries = data.pokemon_entries.map((pokemon: entryData) => ({
+    name: pokemon.pokemon_species.name,
+    url: `https://pokeapi.co/api/v2/pokemon/${pokemon.entry_number}`,
+  }));
+  return { title: "National Pokedex", entries: entries };
 };
